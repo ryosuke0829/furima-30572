4632c95faf0d6f1c615574add03e9e9f651d45c5
ryosuke0829 <ryosuke0829@icloud.com>
2020-11-06 11:44:28 +0900

    レビュー後修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit]
+  before_action :move_to_index, only: [:new]
   def index
     @items = Item.includes(:user)
   end
