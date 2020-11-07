493c9411fe37b6445ca11ff590d59642433c6fde
ryosuke0829 <ryosuke0829@icloud.com>
2020-11-07 10:47:35 +0900

    商品情報変種機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user)
@@ -19,8 +19,14 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
+
 
   private
 
